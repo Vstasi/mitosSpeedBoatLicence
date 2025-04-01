dacc84e2440d80ba3fe9d354befe1768c2cd7748
Vstasi <vstasino@uom.edu.gr>
2025-04-01 22:55:34 +0300

    second commit

--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -302,7 +302,7 @@ $("document").ready(function () {
     const errorEnd = document.createElement("h5");
     const error =
       currentLanguage === "greek"
-        ? "Λυπούμαστε αλλά δεν δικαιούστε το δελτίο μετακίνησης ΑΜΕΑ!"
+        ? "αλλά δεν δικαιούστε το δελτίο μετακίνησης ΑΜΕΑ!"
         : "We are sorry but you are not entitled to the transportation card for the disabled!";
     errorEnd.className = "govgr-error-summary";
     errorEnd.textContent = error + " " + message;
